6f9b4ae46821980ece2c9cee971b472c47d19d38
Myth <peteromoniyi010@gmail.com>
2026-06-21 08:58:43 -0700

    Fix Vite config for Cloudflare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     chunkSizeWarningLimit: 1600,
   },
